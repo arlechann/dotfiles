43f2e81b06f58ad5aff1fa286b3284ab0a6884c6
arlechann <dragnov3728@gmail.com>
2023-11-13 10:55:46 +0900

    update

--- lem/init.lisp
+++ lem/init.lisp
@@ -1,3 +1,12 @@
 (in-package :lem)
 
 (lem/line-numbers:toggle-line-numbers)
+(setf *scroll-recenter-p* nil)
+
+(define-command suspend-editor () ()
+  (charms/ll:endwin)
+  (sb-posix:kill    (sb-posix:getpid) sb-unix:sigtstp))
+(define-key *global-keymap* "C-z" 'suspend-editor)
+
+;; Lisp mode
+(lem-lisp-mode:toggle-paren-coloring)
