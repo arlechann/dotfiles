3f8e963d694493336e06f487fcc763edf2ee05a8
Natamura Takuma <takuma.nakamura@aijus.com>
2023-12-04 11:22:18 +0900

    update

--- emacs/init.el
+++ emacs/init.el
@@ -70,8 +70,35 @@
 
 (leaf solarized-theme
   :ensure t
+  :when nil
   :when window-system
   :config (load-theme 'solarized-dark t))
+
+(leaf doom*
+  :when window-system
+  :config
+  (leaf doom-themes
+    :ensure t
+    :custom '((doom-themes-enable-bold . t)
+              (doom-themes-enable-italic . t))
+    :config
+    (load-theme 'doom-dracula t)
+    (doom-themes-visual-bell-config)
+    (doom-themes-neotree-config)
+    (doom-themes-org-config))
+  (leaf doom-modeline
+    :custom '((doom-modeline-buffer-file-name-style . 'truncate-with-project)
+              (doom-modeline-icon . t)
+              (doom-modeline-major-mode-icon . nil)
+              (doom-modeline-minor-modes . nil))
+    :hook '((after-init . doom-modeline-mode))
+    :config
+    (line-number-mode 0)
+    (column-number-mode 0)
+    (doom-modeline-def-modeline
+     'main
+     '(bar workspace-number window-number evil-state god-state ryo-modal xah-fly-keys matches buffer-info remote-host buffer-position parrot selection-info)
+     '(misc-info persp-name lsp github debug minor-modes input-method major-mode process vcs checker))))
 
 (leaf company
   :ensure t
